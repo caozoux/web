432975f5e0980e3f8f7ee083fe5f8e9572a057ea
czou <cao.zou@windriver.com>
2016-06-24 17:37:13 +0800

    update
    Signed-off-by: czou <cao.zou@windriver.com>

--- java/baseWeb/com/baseWeb/baseServlet.java
+++ java/baseWeb/com/baseWeb/baseServlet.java
@@ -1,11 +1,14 @@
 package com.baseWeb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class baseServlet
@@ -13,12 +16,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/baseServlet")
 public class baseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private List<String> list;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public baseServlet() {
         super();
+        list =new ArrayList<String>();
         // TODO Auto-generated constructor stub
     }
 
@@ -27,7 +31,21 @@ public class baseServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//response.getWriter().append("Served at: ").append(request.getParameter("first_name"));
+		// 设置响应内容类型
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String param_str = request.getParameter("first_name");
+		list.add(param_str);
+
+
+      	for(int i = 0; i < list.size(); i++)  
+        {  
+            String str = list.get(i);  
+			out.println("<h1>" + str + "</h1>");
+        }  
 	}
 
 	/**
